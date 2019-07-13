1f51e729e6e13bbd4a35e91f87136d8a7166e51b
Carlos <miklo.concepcion@gmail.com>
2019-07-12 21:00:15 -0500

    sprint commit

--- README.md
+++ README.md
@@ -29,14 +29,23 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager
 
 1. Describe the biggest difference between `.forEach` & `.map`.
+.forEach calls a provided function on each element in your array. This callback is allowed to mutate the calling array.
+.map call a provided function on every element in the array. The difference is that .map utilizes return values and actually returns a new Array of the same size.
 
 2. What is the difference between a function and a method?
+Both are functions in javascript. A method is a function that is a property of an object. 
 
-3. What is closure?
+3. What is closure? 
 
 4. Describe the four rules of the 'this' keyword.
+A) Window/Global Object Binding - When in the global scope, the value of “this” will be the window/console Object;
+B) Implicit Binding - Whenever a function is called by a preceding dot, the object before that dot is this.
+C) New Binding - Whenever a constructor function is used, this reers to the speific instance of the onject that is created and returned by the constructor.
+D)Explicit Binding - Whenever JavaScript’s call or apply method is used, this is explicitly defined.
 
 5. Why do we need super() in an extended class?
+The super keyword is used to access and call functions on an object's parent.
+The super.prop and super expressions are valid in any method definition in both classes and object literals.
 
 ## Project Set up
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,12 +5,26 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+// const tyrannosaurus = new Object(); 
+//   dinoSaurs.name = 'tyrannosaurus'; 
+//   dinoSaurs.diet = 'carnivorous'; 
+//   dinoSaurs.weight = '7000kg'; 
+//   dinoSaurs.length = '2m';
+//   dinoSaurs.period = 'Late Cretaceous';
 
-// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+//   const stegosaurus = new Object(); 
+//   dinoSaurs.name = 'stegosaurus'; 
+//   dinoSaurs.diet = 'herbivorous'; 
+//   dinoSaurs.weight = '2000kg'; 
+//   dinoSaurs.length = '9m';
+//   dinoSaurs.period = 'Late Jurassic';
 
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+//   const velociraptor = new Object(); 
+//   dinoSaurs.name = 'velociraptor'; 
+//   dinoSaurs.diet = 'carnivorous'; 
+//   dinoSaurs.weight = '15kg'; 
+//   dinoSaurs.length = '1.8m';
+//   dinoSaurs.period = 'Late Cretaceous';
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -35,22 +49,24 @@ console.log();
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
 
-const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+// const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+// {"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+// {"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+// {"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+// {"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+// {"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+// {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+// {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+// {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+// {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+// const universities = [];
+// graduates.map(graduate => universities.push(graduates.university));
+
+// console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -58,13 +74,15 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
-console.log(contactInfo);
+// const contactInfo = [];
+
+// console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
-console.log(uni);
+// const uni = [];
+
+// console.log(uni);
 
 
 // ==== ADVANCED Array Methods ====
@@ -72,7 +90,7 @@ console.log(uni);
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
 
-zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
+let zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
 {"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
 {"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
 {"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
@@ -88,8 +106,11 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
-console.log(animalNames);
+// const zooAnimals = [];
+
+zooAnimals.forEach((animal) => zooAnimals.push(`${animal.animal_name}, ${animal.scientific_name}`));
+
+console.log(zooAnimals);
 
 /* Request 2: .map()    
 
@@ -98,6 +119,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -106,6 +128,7 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const lowerPopulation = [];
+
 console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
@@ -114,6 +137,7 @@ The zoos need to know their total animal population across the United States.  F
 
 */
 const populationTotal = 0;
+
 console.log(populationTotal);
 
 
